ae85f65e701151491fd2b6845bfbe89b4e636c51
Khadim NDao <bayecode5991@gmail.com>
2024-06-24 10:25:36 +0000

    first commit

---
+++ src/main/java/com/byfallcode/byfallsecurity/ByfallsecurityApplication.java
@@ -0,0 +1,13 @@
+package com.byfallcode.byfallsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ByfallsecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ByfallsecurityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/byfallcode/byfallsecurity/ByfallsecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.byfallcode.byfallsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ByfallsecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
