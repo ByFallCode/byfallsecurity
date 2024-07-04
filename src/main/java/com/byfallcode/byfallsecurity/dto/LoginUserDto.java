8362ac26d0d30da195c5b2fce11f1111ff874f71
Khadim NDao <bayecode5991@gmail.com>
2024-07-04 15:52:23 +0000

    security done

---
+++ src/main/java/com/byfallcode/byfallsecurity/dto/LoginUserDto.java
@@ -0,0 +1,14 @@
+package com.byfallcode.byfallsecurity.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LoginUserDto {
+    private String email;
+
+    private String password;
+
+    // getters and setters here...
+}
